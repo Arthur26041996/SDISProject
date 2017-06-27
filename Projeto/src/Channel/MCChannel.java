b5c23b51a0f505e7a51503eab2247da6dda7fe0d
albaNoodle <albiclevi@gmail.com>
2017-06-27 23:59:41 +0100

    Restore

--- Projeto/src/Channel/MCChannel.java
+++ Projeto/src/Channel/MCChannel.java
@@ -2,20 +2,23 @@ package Channel;
 
 import Peer.CustomFileSystem;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class MCChannel implements Runnable
 {
-    private String IP;
+    private final String IP;
     public int PORT;
     public InetAddress group;
     public MulticastSocket mcst;
+    private boolean chunkRec;
 
     public MCChannel(String IP, int PORT) throws UnknownHostException, IOException
     {
@@ -82,6 +85,63 @@ public class MCChannel implements Runnable
                             ex.printStackTrace();
                         }
                         break;
+                        
+                    case "GETCHUNK":
+                        CustomFileSystem a = new CustomFileSystem();
+                        try
+                        {       
+                                //Mira si existe, sino nullpointerexception
+                                File file = new File(Paths.get(".").toAbsolutePath().normalize().toString() + "\\FileSystem" + 
+                                        "\\Peer_" + Peer.Peer.getPeerId() + "\\"+ 
+                                        header[3].trim() + "\\Chunk_" + header[4].trim() + ".txt");
+                                 
+                                //Crear datagrama con el chunk
+                                byte[] bufHeader;
+                                byte[] bufMsg;
+                                DatagramPacket pack;
+                                
+                                
+                                String msgHeader = "CHUNK " + 
+                                Peer.Peer.getProtVersion() + " " +
+                                Peer.Peer.getPeerId() + " " + 
+                                header[3].trim() + " " +
+                                header[4].trim() + " " +
+                                "\r\n\r\n" +
+                                "\r\n\r\n";
+                                bufHeader=msgHeader.getBytes();
+                                
+                                //Transforma el file en array de bits
+                                FileInputStream fis = new FileInputStream(file);
+                                byte chunk[] = new byte[(int)file.length()];
+                                fis.read(chunk);
+                                
+                                bufMsg=new byte[bufHeader.length + chunk.length];
+                                 
+                                
+                                //public static void arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+                                System.arraycopy(bufHeader, 0, bufMsg, 0, bufHeader.length);
+                                System.arraycopy(chunk, 0, bufMsg, bufHeader.length, chunk.length);
+                                
+                                //datagrama preparado para enviar por mdr
+                                pack = new DatagramPacket(bufMsg, bufMsg.length, Peer.Peer.mdr.group,Peer.Peer.mdr.PORT);
+                                
+                    try {
+                        //Esperar entre 0 y 400
+                        Thread.sleep(100);
+//int sleep = (int) (Math.random() * 400);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(MCChannel.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                                
+                                
+                                Peer.Peer.mdr.mcst.send(pack);
+                                 
+                        }
+                        catch(NullPointerException ex)
+                        {
+                            ex.printStackTrace();
+                        }
+                        break;    
                 }
             }
         }
--- Projeto/src/Channel/MDRChannel.java
+++ Projeto/src/Channel/MDRChannel.java
@@ -1,6 +1,9 @@
 package Channel;
 
+import Handlers.FileHandler;
+import Peer.Chunk;
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
@@ -8,9 +11,9 @@ import java.net.UnknownHostException;
 public class MDRChannel implements Runnable
 {
     private String IP;
-    private int PORT;
-    private InetAddress group;
-    private MulticastSocket mcst;
+    public int PORT;
+    public InetAddress group;
+    public MulticastSocket mcst;
 
     public MDRChannel(String IP, int PORT) throws UnknownHostException, IOException
     {
@@ -27,11 +30,75 @@ public class MDRChannel implements Runnable
     {
         try
         {
+            //Join Multicast Channel
             mcst.joinGroup(group);
+            
+            int chunkSize = 64000;
+            //As the package received contains the message Header plus its body,
+            //which consists of the chunk with a (maximum) size of 64Kb, the buffer
+            //will need more than 64Kb to store the whole message.
+            byte[] buff = new byte[chunkSize + 1000];
+            DatagramPacket dp = new DatagramPacket(buff, buff.length);
+            FileHandler fh  = new FileHandler();
+            
+            while(true)
+            {
+                mcst.receive(dp);
+                
+                String message = new String(dp.getData(), 0, dp.getLength());
+                
+                //Split the message into its components Header and Body.
+                //What separates both of them are the <CRLF> characters.
+                String[] msgComponents = message.split("\r\n\r\n");
+                String msgHeader = msgComponents[0]; //SI ES NECESARIO, para el num de chunk y el file id
+                
+                
+                //Create the chunk
+                //0 - <MESSAGE_TYPE>
+                //1 - <VERSION>
+                //2 - <SENDER_ID>
+                //3 - <FILE_ID>
+                //4 - <CHUNK_NO>
+                //5 - <REP_DEGREE>
+                String[] header = msgHeader.split(" ");
+                
+                //alternative for the loopback mode not working when peers
+                //are running on the same machine. Compares the sender ID with
+                //the receiver ID. If it's a match, ignore the package.
+                if((header[2]).equals(Integer.toString(Peer.Peer.getPeerId())))
+                    return;
+                
+                System.out.println("------------------------------------------------------");
+                System.out.println("Message received: \n"+msgHeader);
+                System.out.println("------------------------------------------------------\n");
+                
+                switch(header[0])
+                {
+                    case "CHUNK":
+                        byte [] chunk;
+                        if(msgComponents.length>2){
+                     chunk = msgComponents[2].getBytes();
+                        }
+                        else{chunk=("".getBytes());}
+                       
+                    Chunk novoChunk= new Chunk(header[3], Integer.parseInt(header[4]), chunk);
+                    int msToSleep = 1000;
+                    Thread.sleep(msToSleep);
+                    System.out.println("vuelvo al peer");
+                    Peer.Peer.chunkListRestore(novoChunk);
+                    
+                }
+            }
         }
         catch (IOException ex)
         {
-            
+            System.out.println("[MDBCHANNEL]: Error receiving package!\nMESSAGE: "+ex.getMessage());
+            ex.printStackTrace();
+        }
+        catch (InterruptedException ex)
+        {
+            System.out.println("[MDBCHANNEL]: Error in Thread!\nMESSAGE: "+ex.getMessage());
+            ex.printStackTrace();
         }
     }
     
